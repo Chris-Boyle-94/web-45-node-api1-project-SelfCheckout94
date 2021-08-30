c14e27e61b5ea0db9644780f3f2a50fd6b5aa18d
Christopher Boyle <cboyle824@gmail.com>
2021-08-30 11:59:59 -0400

    successfully grabbing all users using a get request to /api/users

--- api/server.js
+++ api/server.js
@@ -1,6 +1,21 @@
 // BUILD YOUR SERVER HERE
 const express = require("express");
+const User = require("./users/model");
 
 const server = express();
+
+server.use(express.json());
+
+server.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({
+      message: "ERROR: 500. Internal Server Error",
+      error: err.message,
+    });
+  }
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const server = require("./api/server");
 
-// const port = 5000;
-
 // START YOUR SERVER HERE
 
 server.listen(5000, () => {
